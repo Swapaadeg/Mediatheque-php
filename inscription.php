22f5f41fbd534c1753407a2629339a31d575d55f
Marie Rivier <marie.rivier23@gmail.com>
2025-05-28 17:14:18 +0200

    hashage mdp, ne fonctionne pas

--- inscription.php
+++ inscription.php
@@ -1,17 +1,19 @@
 <?php ob_start(); 
 $bdd = new PDO('mysql:host=mysql;dbname=mediatheque;charset=utf8', 'root', 'root');
 //Create
-if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['prenom'])) {
-$nom = htmlspecialchars($_POST['nom']);
-$prenom = htmlspecialchars($_POST['prenom']); 
-$mdp = htmlspecialchars($_POST['mdp']);
-$create = $bdd->prepare('INSERT INTO user(prenom,nom, mdp) VALUES(?, ?, ?)');
-$data = $create->execute(array($prenom, $nom, $mdp));
+
+if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['mdp'])) {
+    $nom = htmlspecialchars($_POST['nom']);
+    $prenom = htmlspecialchars($_POST['prenom']); 
+    $password = htmlspecialchars($_POST['mdp']);
+    $passwordArgon = password_hash($password,PASSWORD_ARGON2I);
+
+    $create = $bdd->prepare('INSERT INTO user(prenom,nom,mdp) VALUES(?, ?, ?)');
+    $data = $create->execute(array($prenom, $nom, $passwordArgon));
 }
 
-$passwordCrypted = sha1(sha1($mdp));
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,7 +25,7 @@ $passwordCrypted = sha1(sha1($mdp));
     <form action="index.php" method="post" >
         <input type="text" name="nom" placeholder="Votre nom">
         <input type="text" name="prenom" placeholder="Votre prénom">
-        <input type="text" name="mdp" placeholder="Mot de passe">
+        <input type="password" name="mdp" placeholder="Mot de passe">
         <button type="submit">S'inscrire</button>
     </form>
 </body>
